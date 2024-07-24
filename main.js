ac0d102fa123736fd51525870c9e30cfa9fed0b1
Abdul latif25 <alatiff045@gmail.com>
2024-07-24 08:21:37 +0700

    update

--- main.js
+++ main.js
@@ -12,22 +12,23 @@ import {
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCdgAITXTldockRB_wgxOrbNUPKzSVBhDs",
-    authDomain: "insan-cemerlang-c9554.firebaseapp.com",
-    projectId: "insan-cemerlang-c9554",
-    storageBucket: "insan-cemerlang-c9554.appspot.com",
-    messagingSenderId: "753628555075",
-    appId: "1:753628555075:web:7a72b2d1e8ae89716931f6",
-    measurementId: "G-KMJZ5V0B8H"
-  };
+  apiKey: "AIzaSyCdgAITXTldockRB_wgxOrbNUPKzSVBhDs",
+  authDomain: "insan-cemerlang-c9554.firebaseapp.com",
+  projectId: "insan-cemerlang-c9554",
+  storageBucket: "insan-cemerlang-c9554.appspot.com",
+  messagingSenderId: "753628555075",
+  appId: "1:753628555075:web:7a72b2d1e8ae89716931f6",
+  measurementId: "G-KMJZ5V0B8H"
+};
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function ambilDaftarAbsensi() {
-  const refDokumen = collection(db, "absensi");
+  const refDokumen = collection(db, "absensi_siswa");
   const kueri = query(refDokumen, orderBy("nama"));
   const cuplikanKueri = await getDocs(kueri);
 
@@ -39,7 +40,7 @@ export async function ambilDaftarAbsensi() {
       nis: dok.data().nis,
       nama: dok.data().nama,
       alamat: dok.data().alamat,
-      notlpnlpn: dok.data().notlpn,
+      notlpn: dok.data().notlpn,
       kelas: dok.data().kelas,
       keterangan: dok.data().keterangan,
       
@@ -57,12 +58,12 @@ export function formatAngka(x) {
 
 export async function tambahAbsensi(tanggal, nis, nama, alamat, notlpn, kelas, keterangan) {
   try {
-    const dokRef = await addDoc(collection(db, 'absensi'), {
+    const dokRef = await addDoc(collection(db, 'absensi_siswa'), {
       tanggal: tanggal,
       nis: nis,
       nama: nama,
       alamat: alamat,
-      notlpnn: notlpn,
+      notlpn: notlpn,
       kelas: kelas,
       keterangan: keterangan
     });
@@ -73,10 +74,10 @@ export async function tambahAbsensi(tanggal, nis, nama, alamat, notlpn, kelas, k
 }
 
 export async function hapusAbsensi(docId) {
-  await deleteDoc(doc(db, "absensi", docId));
+  await deleteDoc(doc(db, "absensi_siswa", docId));
 }
 
-export async function ubahPembeli(docId, nama, alamat, notlpn) {
+export async function ubahPembeli(docId, nama, alamat, noTlpn) {
   await updateDoc(doc(db, "pembeli", docId), {
     nama: nama,
     alamat: alamat,
@@ -88,5 +89,5 @@ export async function ambilPembeli(docId) {
   const docRef = await doc(db, "pembeli", docId);
   const docSnap = await getDoc(docRef);
 
-  return await docSnap.data(); 
+  return await docSnap.data();
 }
